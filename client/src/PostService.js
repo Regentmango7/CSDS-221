1f803a85d899863de014fe4aee75fea6bb781c10
Regentmango7 <regentmango7@gmail.com>
2022-11-17 23:59:12 -0500

    bawling

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -3,5 +3,20 @@ import axios from axios;
 const url='api/posts';
 
 class PostService{
-    
+    static getPosts(){
+        return new Promise(async (resolve, reject) =>{
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                )
+            } catch(err){
+                reject(err);
+            }
+        })
+    }
 }
