cb18a489ba6f6bd318c269f16d379acd75e10373
Regentmango7 <regentmango7@gmail.com>
2022-11-17 23:16:35 -0500

    Starting fresh

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,86 +1,17 @@
 <template>
-  <div id="app">
-    <Table
-      :data="tableData"
-      @openDialogue="openDialogue($event)"
-      @remove="remove($event)"
-    />
-    <transition name="fade">
-      <Dialogue
-        v-if="show"
-        :rowList="tableData"
-        :currentData="curData"
-        @closeDialogue="closeDialogue($event)"
-      />
-    </transition>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Table from './components/Table.vue';
-import Dialogue from './components/dialogue.vue';
-import toastr from 'toastr';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Table,
-    Dialogue,
-  },
-  data: (global) => ({
-    picked: '',
-    show: false,
-    tableData: [],
-    curData: {
-      title: '',
-      description: '',
-      deadline: '',
-      priority: 'Low',
-      isNotComplete: true,
-      new: true,
-    },
-  }),
-  methods: {
-    openDialogue(current) {
-      this.curData = current;
-      this.show = true;
-    },
-    closeDialogue(valid) {
-      this.show = false;
-      if (valid) {
-        if (this.curData.new == true) {
-          this.curData.new = false;
-          this.tableData.push(this.curData);
-          toastr.success('You have successfully added an item.');
-        } else {
-          this.tableData[
-            this.tableData
-              .map((object) => object.title)
-              .indexOf(this.curData.title)
-          ] = this.curData;
-          toastr.success('You have successfully updated an item.');
-        }
-      }
-      this.curData = {
-        title: '',
-        description: '',
-        deadline: '',
-        priority: 'Low',
-        isNotComplete: true,
-        new: true,
-      };
-    },
-    remove(title) {
-      if (this.tableData.map((object) => object.title).indexOf(title) > -1) {
-        this.tableData.splice(
-          this.tableData.map((object) => object.title).indexOf(title),
-          1
-        );
-        toastr.success('You have successfully removed an item.');
-      }
-    },
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
@@ -90,33 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-/*modals*/
-.modal-mask {
-  position: fixed;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  transition: opacity 0.3s ease;
-}
-
-.modal-container {
-  width: 400px; /*master width of modals*/
-  margin: 40px auto 0;
-  padding: 20px 30px;
-  background-color: white;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
+  margin-top: 60px;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,4 @@
-//imports
-  import vue from 'vue';
-  import vuetify from 'vuetify';
-  import router from 'vue-router';
-  import paths from './routing.config';
-  import app from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-//use
-  vue.use(router);
-  vue.use(vuetify);
-  vue.config.productionTip = false
-  new vue({
-    router: new router({routes: paths}),
-    vuetify: new vuetify(),
-    render: h => h(app)
-  }).$mount('#app')
+createApp(App).mount('#app')
